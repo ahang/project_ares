08a20f8716ea63c4eb533bbe2e28ceeb7e1098eb
Andy Hang <hang.andy.t@gmail.com>
2017-03-15 11:29:18 -0500

    checking username

--- assets/javascript/auth.js
+++ assets/javascript/auth.js
@@ -34,20 +34,21 @@
 });
 
     var user = firebase.auth().currentUser;
+    var name, email, photoUrl, uid, emailVerified;
 
     if (user != null) {
-        user.providerData.forEach(function (profile) {
-            console.log("Sign-in provider: "+profile.providerId);
-            //console.log("  Provider-specific UID: "+profile.uid);
-            console.log("  Name: "+profile.displayName);
-            //console.log("  Email: "+profile.email);
-            //console.log("  Photo URL: "+profile.photoURL);
-        });
+        name = user.displayName;
+        email = user.email;
+        photoUrl = user.photoURL;
+        emailVerified = user.emailVerified;
+        uid = user.uid;  // The user's ID, unique to the Firebase project. Do NOT use
+                   // this value to authenticate with your backend server, if
+                   // you have one. Use User.getToken() instead.
     }
 
 
     var uiConfig = {
-        signInSuccessUrl: console.log("The user " + user + " has successfully signed in"),
+        signInSuccessUrl: console.log("The user " + name + " has successfully signed in"),
         signInOptions: [
           // Leave the lines as is for the providers you want to offer your users.
           firebase.auth.GoogleAuthProvider.PROVIDER_ID
