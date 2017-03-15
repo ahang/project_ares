25d9dced874ba9e12c081a50a57d5c25906ceb1c
dashaman <hang.andy.t@gmail.com>
2017-03-14 19:19:00 -0500

    google test

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,16 @@
  $(document).ready(function() {
 
-	// Initialize Firebase
-	var config = {
-		apiKey: "AIzaSyAAJtv2XWoOxQ_1czEw3u6DR5m8OTwC2Qo",
-		authDomain: "projares-b990d.firebaseapp.com",
-		databaseURL: "https://projares-b990d.firebaseio.com",
-		storageBucket: "projares-b990d.appspot.com",
-		messagingSenderId: "811073714607"
-	};
+	// // Initialize Firebase
+	// var config = {
+	// 	apiKey: "AIzaSyAAJtv2XWoOxQ_1czEw3u6DR5m8OTwC2Qo",
+	// 	authDomain: "projares-b990d.firebaseapp.com",
+	// 	databaseURL: "https://projares-b990d.firebaseio.com",
+	// 	storageBucket: "projares-b990d.appspot.com",
+	// 	messagingSenderId: "811073714607"
+	// };
 
-	firebase.initializeApp(config);
+	// firebase.initializeApp(config);
 
-	var database = firebase.database();
+	// var database = firebase.database();
 
 });
---
+++ assets/javascript/auth.js
@@ -0,0 +1,53 @@
+
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyAAJtv2XWoOxQ_1czEw3u6DR5m8OTwC2Qo",
+        authDomain: "projares-b990d.firebaseapp.com",
+        databaseURL: "https://projares-b990d.firebaseio.com",
+        storageBucket: "projares-b990d.appspot.com",
+        messagingSenderId: "811073714607"
+    };
+
+    firebase.initializeApp(config);
+
+    //var database = firebase.database();
+
+    var provider = new firebase.auth.GoogleAuthProvider();
+
+    provider.addScope('https://www.googleapis.com/auth/plus.login');
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+    // ...
+    }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+    // ...
+    });
+
+    var uiConfig = {
+        signInSuccessUrl: console.log("This is successful!"),
+        signInOptions: [
+          // Leave the lines as is for the providers you want to offer your users.
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+          firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+          firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+          firebase.auth.GithubAuthProvider.PROVIDER_ID,
+          firebase.auth.EmailAuthProvider.PROVIDER_ID
+        ],
+        // Terms of service url.
+        tosUrl: console.log("This is successful!")
+      };
+
+      // Initialize the FirebaseUI Widget using Firebase.
+      var ui = new firebaseui.auth.AuthUI(firebase.auth());
+      // The start method will wait until the DOM is loaded.
+      ui.start('#firebaseui-auth-container', uiConfig);
+
