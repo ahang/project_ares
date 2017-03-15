1476efd35004bd2d5947192bc0cdfbebde8a1e90
Andy Hang <hang.andy.t@gmail.com>
2017-03-15 11:06:17 -0500

    login.js update

--- assets/javascript/auth.js
+++ assets/javascript/auth.js
@@ -14,7 +14,7 @@
     //var database = firebase.database();
 
     var provider = new firebase.auth.GoogleAuthProvider();
-
+    
     provider.addScope('https://www.googleapis.com/auth/plus.login');
     firebase.auth().signInWithPopup(provider).then(function(result) {
         // This gives you a Google Access Token. You can use it to access the Google API.
@@ -31,10 +31,23 @@
         // The firebase.auth.AuthCredential type that was used.
         var credential = error.credential;
     // ...
-    });
+});
+
+    var user = firebase.auth().currentUser;
+
+    if (user != null) {
+        user.providerData.forEach(function (profile) {
+            console.log("Sign-in provider: "+profile.providerId);
+            //console.log("  Provider-specific UID: "+profile.uid);
+            console.log("  Name: "+profile.displayName);
+            //console.log("  Email: "+profile.email);
+            //console.log("  Photo URL: "+profile.photoURL);
+        });
+    }
+
 
     var uiConfig = {
-        signInSuccessUrl: "/movie.html",
+        signInSuccessUrl: console.log("The user " + user + " has successfully signed in"),
         signInOptions: [
           // Leave the lines as is for the providers you want to offer your users.
           firebase.auth.GoogleAuthProvider.PROVIDER_ID
