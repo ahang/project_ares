1f2ad083c4e12da93df43be415c78bc083105697
ahang1 <hang.andy.t@gmail.com>
2017-03-15 18:30:13 -0500

    login fix

--- assets/javascript/auth.js
+++ assets/javascript/auth.js
@@ -74,7 +74,7 @@
             var providerData = user.providerData;
             user.getToken().then(function(accessToken) {
                 $(".login-container").addClass("invisible");
-                $(".userContainer").removeClass("invisible");
+                $(".user-container").removeClass("invisible");
                 $(".userName").html("Welcome " + displayName);
             // document.getElementById('sign-in-status').textContent = 'Signed in';
             // document.getElementById('sign-in').textContent = 'Sign out';
