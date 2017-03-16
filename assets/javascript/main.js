3729b2267b2dc04167107e8fc38b354c52518c44
foxmessenger <iamliamfox@gmail.com>
2017-03-15 21:40:38 -0500

    both api are working, we just need to figure out how to bounce back if we don't see the movie

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -6,31 +6,58 @@
         // We're optionally using a form so the user may hit Enter to search instead of clicking the button
         event.preventDefault();
 
-        // Here we grab the text from the input box
-        var movie = $("#movie-input").val();
-        var apiKey = "395604c82b2663c214732886fe58d756";
-        var apiReadAccessToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzOTU2MDRjODJiMjY2M2MyMTQ3MzI4ODZmZTU4ZDc1NiIsInN1YiI6IjU4YzljYzJkYzNhMzY4NDEyYTAwMDQ3YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.B6rxV4ke7MA56btQq1wNVTJYcjPx335Oi7C4gNArQF0";
+            // Here we grab the text from the input box
+            var movie = $("#movie-input").val();
+            var apiKey = "395604c82b2663c214732886fe58d756";
+            var apiReadAccessToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzOTU2MDRjODJiMjY2M2MyMTQ3MzI4ODZmZTU4ZDc1NiIsInN1YiI6IjU4YzljYzJkYzNhMzY4NDEyYTAwMDQ3YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.B6rxV4ke7MA56btQq1wNVTJYcjPx335Oi7C4gNArQF0";
 		// the website page
 		var	apiRequest = "https://api.themoviedb.org/";
 		// discover a movie by a parameter
 		var discoverURL = "discover/movie";
 		// popularity parameter
-		var popularity = "?sort_by=popularity.desc&api_key="
+		var popularity = "?sort_by=popularity.desc&api_key=";
 
 		// full query request, includes: language is English, popular, non-adult movies, and action genre
-		var fullQUERY = "https://api.themoviedb.org/3/discover/movie?api_key=" + apiKey + "&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=28"
-
+		var fullQUERY = "https://api.themoviedb.org/3/discover/movie?api_key=" + apiKey + "&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=28";
+        
         // Here we construct our URL
         var queryURL = fullQUERY;
 
+        var flixRouletteURL = "http://netflixroulette.net/api/api.php?title=";
 
-        
+
+
 
         //this will call theMovieDB movie list 
         $.ajax({
         	url: queryURL,
         	method: 'GET'
         }).then(function(response){
-        	$('#movie-view').html("<p>" + JSON.stringify(response) + "</p>");
+        	// return response;
+            // $('#movie-view').html("<p>" + JSON.stringify(response.results[0].title + "</p>");
+            var title = response.results[8].title;
+            title = encodeURIComponent(title);
+            
+            flixRoulette(title);            
         });
+
+
+        function flixRoulette(movieTitle) {
+
+            flixRouletteURL += movieTitle;
+            $.ajax({
+                url: flixRouletteURL,
+                method: 'GET'
+            }).then(function(response){
+                console.log("this is: " + response);
+                $('#movie-view').html("<p>" + JSON.stringify('<img src="' + 'XXX' + '">'));
+                console.log("this is: " + response.poster);
+            
+            });
+        };
+    });
 });
+
+
+
+
