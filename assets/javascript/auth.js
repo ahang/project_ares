cede1b9fa4ed61d4b90dd8c0c1a82e615ef7099d
dashaman <hang.andy.t@gmail.com>
2017-03-16 19:10:41 -0500

    fix for google login

--- assets/javascript/auth.js
+++ assets/javascript/auth.js
@@ -36,20 +36,20 @@
     // ...
     });
 
-    // var uiConfig = {
-    //     signInSuccessUrl: "null",
-    //     signInOptions: [
-    //       // Leave the lines as is for the providers you want to offer your users.
-    //       firebase.auth.GoogleAuthProvider.PROVIDER_ID
-    //     ],
-    //     // Terms of service url.
-    //     tosUrl: console.log("This is successful!")
-    //   };
+    var uiConfig = {
+        signInSuccessUrl: false,
+        signInOptions: [
+          // Leave the lines as is for the providers you want to offer your users.
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID
+        ],
+        // Terms of service url.
+        tosUrl: console.log("This is successful!")
+      };
 
-    //   // Initialize the FirebaseUI Widget using Firebase.
-    //   var ui = new firebaseui.auth.AuthUI(firebase.auth());
-    //   // The start method will wait until the DOM is loaded.
-    //   ui.start('#firebaseui-auth-container', uiConfig);
+      // Initialize the FirebaseUI Widget using Firebase.
+      var ui = new firebaseui.auth.AuthUI(firebase.auth());
+      // The start method will wait until the DOM is loaded.
+      ui.start('#firebaseui-auth-container', uiConfig);
 
 
     initApp = function() {
