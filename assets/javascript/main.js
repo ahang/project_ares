d515caad40bd5de157229567e0bcb1a42c50f017
foxmessenger <iamliamfox@gmail.com>
2017-03-18 14:01:46 -0500

    added a random number generator: working code

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -18,40 +18,45 @@
          var popularity = "?sort_by=popularity.desc&api_key=";
 
          // full query request, includes: language is English, popular, non-adult movies, and action genre
-         var fullQUERY = "https://api.themoviedb.org/3/discover/movie?api_key=" + apiKey + "&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=28";
+         var fullQUERY = "https://api.themoviedb.org/3/discover/movie?api_key=" + apiKey + "&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=5&with_genres=28";
 
          // Here we construct our URL
          var queryURL = fullQUERY;
 
          //var flixRouletteURL = "http://netflixroulette.net/api/api.php?title=";
 
-
-
-
          //this will call theMovieDB movie list
          $.ajax({
              url: queryURL,
              method: 'GET'
          }).then(function(response) {
-             // return response;
-             // $('#movie-view').html("<p>" + JSON.stringify(response.results[0].title + "</p>");
-             var title = response.results[10].title;
-             
-             console.log(title);
-             flixRoulette(title);
+            // random number generator
+            function randomOrder() {
+                return Math.floor(Math.random() * 20);
+            }
+
+            var randomOrder = randomOrder();
+            
+            // return response;
+            // $('#movie-view').html("<p>" + JSON.stringify(response.results[0].title + "</p>");
+            var title = response.results[randomOrder].title;
+
+            console.log(title);
+            flixRoulette(title);
          });
+
 
 
          function flixRoulette(movieTitle) {
              var flixRouletteURL = encodeURIComponent(movieTitle);
-            
-              var flixSearch = "https://unogs-unogs-v1.p.mashape.com/api.cgi?q=" + flixRouletteURL + "-!1900,2017-!0,5-!0,10-!0-!Any-!Any-!Any-!gt100-!{downloadable}&t=ns&cl=all&st=adv&ob=Relevance&p=1&sa=and"
 
-            console.log(flixSearch);
+             var flixSearch = "https://unogs-unogs-v1.p.mashape.com/api.cgi?q=" + flixRouletteURL + "-!1900,2017-!0,5-!0,10-!0-!Any-!Any-!Any-!gt100-!{downloadable}&t=ns&cl=all&st=adv&ob=Relevance&p=1&sa=and"
+
+             console.log(flixSearch);
              $.ajax({
                  beforeSend: function(request) {
-                    request.setRequestHeader("X-Mashape-Key", "f4wdRtwlPImshDfHb3hczt25D4bGp1HrZTFjsnhctrypL2Qe6I");
-                    request.setRequestHeader("Accept", "application/json");
+                     request.setRequestHeader("X-Mashape-Key", "f4wdRtwlPImshDfHb3hczt25D4bGp1HrZTFjsnhctrypL2Qe6I");
+                     request.setRequestHeader("Accept", "application/json");
                  },
                  url: flixSearch,
                  method: 'GET'
@@ -59,14 +64,16 @@
                  console.log(response.ITEMS[0][1]);
                  $("#movie-view").html("<p>Your selected movie is " + response.ITEMS[0][1]);
                  console.log("Title");
-                 $("#movie-view").append("<br>" + '<img src="'+ response.ITEMS[0][2] + '"/>');
+                 $("#movie-view").append("<br>" + '<img src="' + response.ITEMS[0][2] + '"/>');
                  console.log('this is the image');
                  $("#movie-view").append("<br> The plot of this movie is " + response.ITEMS[0][3]);
                  console.log('this is the plot');
-                // $('#movie-view').html("<p>" + '<img src="' + response + '">');
-                // console.log("this is: " + response.poster);
+                 // $('#movie-view').html("<p>" + '<img src="' + response + '">');
+                 // console.log("this is: " + response.poster);
 
              });
          };
      });
  });
+
+
