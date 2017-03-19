5ac94fd46326248030971c28e28f3ed064fb0346
dashaman <hang.andy.t@gmail.com>
2017-03-19 15:43:51 -0500

    netflix url

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
     $("#find-movie").on("click", function(event) {
-
+        clearInfo();
         // Preventing the submit button from trying to submit the form
         // We're optionally using a form so the user may hit Enter to search instead of clicking the button
         event.preventDefault();
@@ -42,7 +42,7 @@ $(document).ready(function() {
         function flixRoulette(movieTitle) {
             var searchParam = encodeURIComponent(movieTitle);
 
-            var unogsUrl = "https://unogs-unogs-v1.p.mashape.com/api.cgi?q=" + searchParam + "-!1900,2017-!0,5-!0,10-!0-!Any-!Any-!Any-!gt100-!{downloadable}&t=ns&cl=all&st=adv&ob=Relevance&p=1&sa=and"
+            var unogsUrl = "https://unogs-unogs-v1.p.mashape.com/api.cgi?q=" + searchParam + "-!1900,2017-!0,5-!0,10-!0-!Any-!Any-!Any-!gt100-!No&t=ns&cl=all&st=adv&ob=Relevance&p=1&sa=and"
 
             $.ajax({
                 beforeSend: function(request) {
@@ -58,6 +58,8 @@ $(document).ready(function() {
 
                 var netflixBtn = $("<button>");
                 netflixBtn.addClass("btn netflixBtn img");
+                netflixBtn.attr("data-link", response.ITEMS[0][0]);
+                console.log(netflixBtn);
                 netflixBtn.append('<img src="assets/images/netflix.png"/>');
                 //etflix.addText("View it on Netflix");
                 $("#movie-view").append(netflixBtn);
@@ -73,6 +75,20 @@ $(document).ready(function() {
                 $("#movie-view").append(thumbsDwnBtn);
             });
         };
+
+        function clearInfo() {
+            $("movie-view").empty;
+        }
+
+        $(document).on("click", ".netflixBtn", function() {
+            var name = $(this).attr("data-link");
+            var netflixURL = "https://www.netflix.com/title/" + name;
+
+            window.open(netflixURL);
+        });
+
+
+
 
 
 
